abc916786823d1e69ed90ac4609557e6fdf03919
Lina Andersson <lina.karlsen.andersson@gu.se>
2026-09-12 09:57:52 +0200

    Added R script to export large plots + exported Figures

---
+++ Export_Large_Figures.R
@@ -0,0 +1,45 @@
+
+# Export Fig1
+ggsave(
+  filename = "Fig1.png", 
+  plot = Fig1,     # <-- Put your variable name here
+  width = 14,         # Gives a large figure room to stretch out
+  height = 8, 
+  dpi = 300           # Keeps text and lines crystal clear
+)
+
+# Export Fig2
+ggsave(
+  filename = "Fig2.png", 
+  plot = Fig2,     # <-- Put your variable name here
+  width = 14,         # Gives a large figure room to stretch out
+  height = 8, 
+  dpi = 300           # Keeps text and lines crystal clear
+)
+
+# Export Fig3
+ggsave(
+  filename = "Fig3.png", 
+  plot = Fig3,     # <-- Put your variable name here
+  width = 14,         # Gives a large figure room to stretch out
+  height = 8, 
+  dpi = 300           # Keeps text and lines crystal clear
+)
+
+# Export Fig4
+ggsave(
+  filename = "Fig4.png", 
+  plot = Fig4,     # <-- Put your variable name here
+  width = 14,         # Gives a large figure room to stretch out
+  height = 8, 
+  dpi = 300           # Keeps text and lines crystal clear
+)
+
+# Export Fig5
+ggsave(
+  filename = "Fig5.png", 
+  plot = Fig5,     # <-- Put your variable name here
+  width = 14,         # Gives a large figure room to stretch out
+  height = 8, 
+  dpi = 300           # Keeps text and lines crystal clear
+)
